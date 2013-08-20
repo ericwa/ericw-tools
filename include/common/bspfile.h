0092347789ae8a370004b9ff9cc00ae37d49b75f
Kevin Shanahan <kmshanah@disenchant.net>
2013-08-20 12:22:11 +0930

    common: use stdint types in the common bspfile.h
    Signed-off-by: Kevin Shanahan <kmshanah@disenchant.net>

--- include/common/bspfile.h
+++ include/common/bspfile.h
@@ -20,6 +20,8 @@
 #ifndef __COMMON_BSPFILE_H__
 #define __COMMON_BSPFILE_H__
 
+#include <stdint.h>
+
 #include <common/cmdlib.h>
 #include <common/log.h>
 
@@ -49,11 +51,9 @@
 #define BSPVERSION  29
 #define BSP2VERSION (('B' << 24) | ('S' << 16) | ('P' << 8) | '2')
 
-/* ========================================================================= */
-
 typedef struct {
-    int fileofs;
-    int filelen;
+    int32_t fileofs;
+    int32_t filelen;
 } lump_t;
 
 #define LUMP_ENTITIES      0
@@ -85,27 +85,27 @@ typedef struct {
     float mins[3];
     float maxs[3];
     float origin[3];
-    int headnode[MAX_MAP_HULLS];
-    int visleafs;		/* not including the solid leaf 0 */
-    int firstface;
-    int numfaces;
+    int32_t headnode[MAX_MAP_HULLS];
+    int32_t visleafs;		/* not including the solid leaf 0 */
+    int32_t firstface;
+    int32_t numfaces;
 } dmodel_t;
 
 typedef struct {
-    int version;
+    int32_t version;
     lump_t lumps[BSP_LUMPS];
 } dheader_t;
 
 typedef struct {
-    int nummiptex;
-    int dataofs[4];		/* [nummiptex] */
+    int32_t nummiptex;
+    int32_t dataofs[4];		/* [nummiptex] */
 } dmiptexlump_t;
 
 #define MIPLEVELS 4
 typedef struct miptex_s {
     char name[16];
-    unsigned width, height;
-    unsigned offsets[MIPLEVELS];	/* four mip maps stored */
+    uint32_t width, height;
+    uint32_t offsets[MIPLEVELS];	/* four mip maps stored */
 } miptex_t;
 
 typedef struct {
@@ -126,7 +126,7 @@ typedef struct {
 typedef struct {
     float normal[3];
     float dist;
-    int type;
+    int32_t type;
 } dplane_t;
 
 #define CONTENTS_EMPTY -1
@@ -138,23 +138,23 @@ typedef struct {
 
 /* !!! if this is changed, it must be changed in asm_i386.h too !!! */
 typedef struct {
-    int planenum;
-    short children[2];		/* negative numbers are -(leafs+1), not nodes */
-    short mins[3];		/* for sphere culling */
-    short maxs[3];
-    unsigned short firstface;
-    unsigned short numfaces;	/* counting both sides */
+    int32_t planenum;
+    int16_t children[2];	/* negative numbers are -(leafs+1), not nodes */
+    int16_t mins[3];		/* for sphere culling */
+    int16_t maxs[3];
+    uint16_t firstface;
+    uint16_t numfaces;		/* counting both sides */
 } dnode_t;
 
 typedef struct {
-    int planenum;
-    short children[2];		/* negative numbers are contents */
+    int32_t planenum;
+    int16_t children[2];		/* negative numbers are contents */
 } dclipnode_t;
 
 typedef struct texinfo_s {
     float vecs[2][4];		/* [s/t][xyz offset] */
-    int miptex;
-    int flags;
+    int32_t miptex;
+    int32_t flags;
 } texinfo_t;
 
 #define TEX_SPECIAL 1		/* sky or slime, no lightmap or 256 subdivision */
@@ -163,20 +163,20 @@ typedef struct texinfo_s {
  * counterclockwise use of the edge in a face
  */
 typedef struct {
-    unsigned short v[2];	/* vertex numbers */
+    uint16_t v[2];		/* vertex numbers */
 } dedge_t;
 
 #define MAXLIGHTMAPS 4
 typedef struct {
-    short planenum;
-    short side;
-    int firstedge;		/* we must support > 64k edges */
-    short numedges;
-    short texinfo;
+    int16_t planenum;
+    int16_t side;
+    int32_t firstedge;		/* we must support > 64k edges */
+    int16_t numedges;
+    int16_t texinfo;
 
-/* lighting info */
-    byte styles[MAXLIGHTMAPS];
-    int lightofs;		/* start of [numstyles*surfsize] samples */
+    /* lighting info */
+    uint8_t styles[MAXLIGHTMAPS];
+    int32_t lightofs;		/* start of [numstyles*surfsize] samples */
 } dface_t;
 
 /* Ambient Sounds */
@@ -191,15 +191,15 @@ typedef struct {
  * all other leafs need visibility info
  */
 typedef struct {
-    int contents;
-    int visofs;			/* -1 = no visibility info */
-    short mins[3];		/* for frustum culling     */
-    short maxs[3];
+    int32_t contents;
+    int32_t visofs;		/* -1 = no visibility info */
+    int16_t mins[3];		/* for frustum culling     */
+    int16_t maxs[3];
 
-    unsigned short firstmarksurface;
-    unsigned short nummarksurfaces;
+    uint16_t firstmarksurface;
+    uint16_t nummarksurfaces;
 
-    byte ambient_level[NUM_AMBIENTS];
+    uint8_t ambient_level[NUM_AMBIENTS];
 } dleaf_t;
 
 /* ========================================================================= */
@@ -296,10 +296,10 @@ typedef struct {
     dedge_t *dedges;
 
     int nummarksurfaces;
-    unsigned short *dmarksurfaces;
+    uint16_t *dmarksurfaces;
 
     int numsurfedges;
-    int *dsurfedges;
+    int32_t *dsurfedges;
 } bspdata_t;
 
 /* Transitional helper functions */
