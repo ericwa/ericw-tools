ec37b52807cc8fc7e41b0ab7021f1510e10638a3
Eric Wasylishen <ewasylishen@gmail.com>
2016-09-23 00:43:18 -0600

    light: disable "denormals are zero", causing build issues on gcc4.8/linux

--- light/light.cc
+++ light/light.cc
@@ -30,7 +30,7 @@
 
 #ifdef HAVE_EMBREE
 #include <xmmintrin.h>
-#include <pmmintrin.h>
+//#include <pmmintrin.h>
 #endif
 
 #include <memory>
@@ -220,7 +220,7 @@ LightThread(void *arg)
 
 #ifdef HAVE_EMBREE
     _MM_SET_FLUSH_ZERO_MODE(_MM_FLUSH_ZERO_ON);
-    _MM_SET_DENORMALS_ZERO_MODE(_MM_DENORMALS_ZERO_ON);
+//    _MM_SET_DENORMALS_ZERO_MODE(_MM_DENORMALS_ZERO_ON);
 #endif
 
     while (1) {
