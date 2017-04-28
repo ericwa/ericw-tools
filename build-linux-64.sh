701fad8460cce8a2a83e65bdc783e0030d0d594a
Eric Wasylishen <ewasylishen@gmail.com>
2017-04-28 15:35:04 -0600

    build: fix embree path in build-linux64.sh

--- build-linux-64.sh
+++ build-linux-64.sh
@@ -11,7 +11,7 @@ mkdir build
 cd build
 wget https://github.com/embree/embree/releases/download/v2.15.0/embree-2.15.0.x86_64.linux.tar.gz -O embree.tgz
 tar xf embree.tgz
-cmake .. -DCMAKE_BUILD_TYPE=Release -Dembree_DIR="$(pwd)/embree-2.15.0.x86_64.linux/lib/cmake/embree-2.15.0"
+cmake .. -DCMAKE_BUILD_TYPE=Release -Dembree_DIR="$(pwd)/embree-2.15.0.x86_64.linux"
 make -j8 VERBOSE=1
 make -j8 VERBOSE=1 testlight
 cpack
