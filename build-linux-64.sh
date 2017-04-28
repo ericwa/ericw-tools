b0960192377cf19decadc00fa0a466099da91a74
Eric Wasylishen <ewasylishen@gmail.com>
2017-04-28 15:26:40 -0600

    build: update mac and linux for embree 2.15.0

--- build-linux-64.sh
+++ build-linux-64.sh
@@ -9,9 +9,9 @@ cmake --version
 
 mkdir build
 cd build
-wget https://github.com/embree/embree/releases/download/v2.14.0/embree-2.14.0.x86_64.linux.tar.gz -O embree.tgz
+wget https://github.com/embree/embree/releases/download/v2.15.0/embree-2.15.0.x86_64.linux.tar.gz -O embree.tgz
 tar xf embree.tgz
-cmake .. -DCMAKE_BUILD_TYPE=Release -Dembree_DIR="$(pwd)/embree-2.14.0.x86_64.linux/lib/cmake/embree-2.14.0"
+cmake .. -DCMAKE_BUILD_TYPE=Release -Dembree_DIR="$(pwd)/embree-2.15.0.x86_64.linux/lib/cmake/embree-2.15.0"
 make -j8 VERBOSE=1
 make -j8 VERBOSE=1 testlight
 cpack
--- build-osx.sh
+++ build-osx.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 BUILD_DIR=build-osx
-EMBREE_TGZ="https://github.com/embree/embree/releases/download/v2.14.0/embree-2.14.0.x86_64.macosx.tar.gz"
+EMBREE_TGZ="https://github.com/embree/embree/releases/download/v2.15.0/embree-2.15.0.x86_64.macosx.tar.gz"
 EMBREE_TGZ_NAME=$(basename "$EMBREE_TGZ")
 EMBREE_DIR_NAME=$(basename "$EMBREE_TGZ" ".tar.gz")
 EMBREE_WITH_VERSION=$(basename "$EMBREE_TGZ" ".x86_64.macosx.tar.gz")
@@ -15,7 +15,7 @@ mkdir "$BUILD_DIR"
 cd "$BUILD_DIR"
 wget "$EMBREE_TGZ"
 tar xf "$EMBREE_TGZ_NAME"
-EMBREE_CMAKE_DIR="$(pwd)/$EMBREE_DIR_NAME/lib/cmake/$EMBREE_WITH_VERSION"
+EMBREE_CMAKE_DIR="$(pwd)/$EMBREE_DIR_NAME"
 cmake .. -DCMAKE_BUILD_TYPE=Release -Dembree_DIR="$EMBREE_CMAKE_DIR"
 make -j8
 make -j8 testlight
