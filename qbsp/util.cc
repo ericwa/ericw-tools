a53cf8dbbe16fcda571344c1e8836c0fe6756631
Eric Wasylishen <ewasylishen@gmail.com>
2021-08-22 23:45:42 -0600

    qbsp: fix some thread safety bugs (mostly affecting stats output)

--- qbsp/util.cc
+++ qbsp/util.cc
@@ -19,6 +19,7 @@
     See file, 'COPYING', for details.
 */
 
+#include <mutex>
 #include <stddef.h>
 #include <stdarg.h>
 #include <stdlib.h>
@@ -33,6 +34,8 @@ static int rgMemActive[GLOBAL + 1];
 static int rgMemPeak[GLOBAL + 1];
 static int rgMemActiveBytes[GLOBAL + 1];
 static int rgMemPeakBytes[GLOBAL + 1];
+
+std::mutex memoryStatsLock;
 
 /*
 ==========
@@ -74,6 +77,8 @@ AllocMem(int Type, int cElements, bool fZero)
     if (Type == FACE && cElements == 1)
         ((face_t *)pTemp)->planenum = -1;
 
+    std::unique_lock<std::mutex> lck { memoryStatsLock };
+
     rgMemTotal[Type] += cElements;
     rgMemActive[Type] += cElements;
     rgMemActiveBytes[Type] += cSize;
@@ -100,12 +105,14 @@ FreeMem
 void
 FreeMem(void *pMem, int Type, int cElements)
 {
+    free(pMem);
+
+    std::unique_lock<std::mutex> lck { memoryStatsLock };
+
     rgMemActive[Type] -= cElements;
 
     rgMemActiveBytes[Type] -= cElements * MemSize[Type];
     rgMemActive[GLOBAL] -= cElements * MemSize[Type];
-
-    free(pMem);
 }
 
 
