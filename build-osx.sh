ac79487e4d7dfe6255368907371ea6f5ecd08fb6
Eric Wasylishen <ewasylishen@gmail.com>
2021-09-12 01:30:04 -0600

    ci: mac: use embree's official package over homebrew.
    Has @rpath set up already

--- build-osx.sh
+++ build-osx.sh
@@ -1,18 +1,35 @@
 #!/bin/bash
 
 BUILD_DIR=build-osx
+EMBREE_ZIP="https://github.com/embree/embree/releases/download/v3.13.0/embree-3.13.0.x86_64.macosx.zip"
+
+# embree-3.13.1.x86_64.macosx.zip
+EMBREE_ZIP_NAME=$(basename "$EMBREE_ZIP")
+
+# embree-3.13.1.x86_64.macosx
+EMBREE_DIR_NAME=$(basename "$EMBREE_ZIP_NAME" ".zip")
+
+TBB_TGZ="https://github.com/oneapi-src/oneTBB/releases/download/v2021.2.0/oneapi-tbb-2021.2.0-mac.tgz"
+TBB_TGZ_NAME=$(basename "$TBB_TGZ")
+TBB_DIR_NAME="oneapi-tbb-2021.2.0"
 
 if [ -d "$BUILD_DIR" ]; then
   echo "$BUILD_DIR already exists, remove it first"
   exit 1
 fi
 
-brew install embree tbb
-
 mkdir "$BUILD_DIR"
 cd "$BUILD_DIR"
 
-cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH="$(brew --prefix embree);$(brew --prefix tbb)"
+wget "$EMBREE_ZIP"
+unzip "$EMBREE_ZIP_NAME"
+
+wget "$TBB_TGZ"
+tar xf "$TBB_TGZ_NAME"
+
+EMBREE_CMAKE_DIR="$(pwd)/$EMBREE_DIR_NAME/lib/cmake/embree-3.13.0"
+TBB_CMAKE_DIR="$(pwd)/${TBB_DIR_NAME}/lib/cmake"
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH="$EMBREE_CMAKE_DIR;$TBB_CMAKE_DIR"
 make -j8 || exit 1
 make -j8 testlight || exit 1
 make -j8 testqbsp || exit 1
