a1609ae1e6967f32c5686c72037f7a36a973fcd4
Eric Wasylishen <ewasylishen@gmail.com>
2021-10-09 15:22:27 -0600

    qbsp: add _omitbrushes to tell qbsp to discard all brushes in a func_

--- qbsp/brush.cc
+++ qbsp/brush.cc
@@ -1298,6 +1298,11 @@ Brush_LoadEntity(mapentity_t *dst, const mapentity_t *src, const int hullnum)
     const bool func_illusionary_visblocker =
         (0 == Q_strcasecmp(classname, "func_illusionary_visblocker"));
 
+    // _omitbrushes 1 just discards all brushes in the entity.
+    // could be useful for geometry guides, selective compilation, etc.
+    if (atoi(ValueForKey(src, "_omitbrushes")))
+        return;
+
     for (i = 0; i < src->nummapbrushes; i++, mapbrush++) {
         mapbrush = &src->mapbrush(i);
         contentflags_t contents = Brush_GetContents(mapbrush);
