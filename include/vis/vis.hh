f6ba5d77f9679dfc7a746864d0b14f620e237c50
Jonathan <jonno.5000@gmail.com>
2022-05-21 02:45:30 -0400

    allow recompilation of PHS from PVS data via -phsonly

--- include/vis/vis.hh
+++ include/vis/vis.hh
@@ -229,6 +229,7 @@ public:
     setting_scalar visdist{
         this, "visdist", 0.0, &advanced_group, "control the distance required for a portal to be considered seen"};
     setting_bool nostate{this, "nostate", false, &advanced_group, "ignore saved state files, for forced re-runs"};
+    setting_bool phsonly{this, "phsonly", false, &advanced_group, "re-calculate the PHS of a Quake II BSP without touching the PVS"};
 
     fs::path sourceMap;
 
