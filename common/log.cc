54efff2e7a87076eb619139b2d209055da48bda8
Jonathan <jonno.5000@gmail.com>
2022-06-06 03:29:33 -0400

    don't print out % if it hasn't changed

--- common/log.cc
+++ common/log.cc
@@ -112,6 +112,7 @@ void percent(uint64_t count, uint64_t max, bool displayElapsed)
     if (!is_timing) {
         start_time = I_FloatTime();
         is_timing = true;
+        last_count = -1;
     }
 
     if (count == max) {
@@ -120,8 +121,13 @@ void percent(uint64_t count, uint64_t max, bool displayElapsed)
         if (displayElapsed) {
             print(flag::PERCENT, "[100%] time elapsed: {:.3}\n", elapsed);
         }
+        last_count = -1;
     } else {
-        print(flag::PERCENT, "[{:>3}%]\r", static_cast<uint32_t>((static_cast<float>(count) / max) * 100));
+        uint32_t pct = static_cast<uint32_t>((static_cast<float>(count) / max) * 100);
+        if (last_count != pct) {
+            print(flag::PERCENT, "[{:>3}%]\r", pct);
+            last_count = pct;
+        }
     }
 }
 };
