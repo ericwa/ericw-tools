436a9183e72636731ddf880da4e5c4c5922b5eac
Jonathan <jonno.5000@gmail.com>
2022-06-22 23:42:38 -0400

    use aligned allocator for rays

--- include/light/trace_embree.hh
+++ include/light/trace_embree.hh
@@ -52,13 +52,6 @@ public:
 
 public:
     inline raystream_embree_common_t() = default;
-
-    inline raystream_embree_common_t(size_t maxRays)
-        : _rays_maxdist(maxRays), _point_indices(maxRays), _ray_colors(maxRays),
-          _ray_normalcontribs(maxRays), _ray_dynamic_styles(maxRays), _maxrays(maxRays)
-    {
-    }
-
     virtual ~raystream_embree_common_t() = default;
 
     virtual void resize(size_t size)
@@ -111,8 +104,10 @@ extern RTCScene scene;
 
 inline void *q_aligned_malloc(size_t align, size_t size)
 {
-#ifdef _MSC_VER
-    return _aligned_malloc(size, align);
+#ifdef _mm_malloc
+    return _mm_malloc(size, align);
+#elif __STDC_VERSION__ >= 201112L
+    return aligned_alloc(align, size);
 #else
     void *ptr;
     if (0 != posix_memalign(&ptr, align, size)) {
@@ -124,12 +119,155 @@ inline void *q_aligned_malloc(size_t align, size_t size)
 
 inline void q_aligned_free(void *ptr)
 {
-#ifdef _MSC_VER
-    _aligned_free(ptr);
+#ifdef _mm_malloc
+    _mm_free(ptr);
 #else
     free(ptr);
 #endif
 }
+
+
+/**
+ * Allocator for aligned data.
+ *
+ * Modified from the Mallocator from Stephan T. Lavavej.
+ * <http://blogs.msdn.com/b/vcblog/archive/2008/08/28/the-mallocator.aspx>
+ */
+template <typename T, std::size_t Alignment>
+class aligned_allocator
+{
+public:
+	// The following will be the same for virtually all allocators.
+	typedef T * pointer;
+	typedef const T * const_pointer;
+	typedef T& reference;
+	typedef const T& const_reference;
+	typedef T value_type;
+	typedef std::size_t size_type;
+	typedef ptrdiff_t difference_type;
+ 
+	T * address(T& r) const
+	{
+		return &r;
+	}
+ 
+	const T * address(const T& s) const
+	{
+		return &s;
+	}
+ 
+	std::size_t max_size() const
+	{
+		// The following has been carefully written to be independent of
+		// the definition of size_t and to avoid signed/unsigned warnings.
+		return (static_cast<std::size_t>(0) - static_cast<std::size_t>(1)) / sizeof(T);
+	}
+ 
+ 
+	// The following must be the same for all allocators.
+	template <typename U>
+	struct rebind
+	{
+		typedef aligned_allocator<U, Alignment> other;
+	} ;
+ 
+	bool operator!=(const aligned_allocator& other) const
+	{
+		return !(*this == other);
+	}
+ 
+	void construct(T * const p, const T& t) const
+	{
+		void * const pv = static_cast<void *>(p);
+ 
+		new (pv) T(t);
+	}
+ 
+	void destroy(T * const p) const
+	{
+		p->~T();
+	}
+ 
+	// Returns true if and only if storage allocated from *this
+	// can be deallocated from other, and vice versa.
+	// Always returns true for stateless allocators.
+	bool operator==(const aligned_allocator& other) const
+	{
+		return true;
+	}
+ 
+ 
+	// Default constructor, copy constructor, rebinding constructor, and destructor.
+	// Empty for stateless allocators.
+	aligned_allocator() { }
+ 
+	aligned_allocator(const aligned_allocator&) { }
+ 
+	template <typename U> aligned_allocator(const aligned_allocator<U, Alignment>&) { }
+ 
+	~aligned_allocator() { }
+ 
+ 
+	// The following will be different for each allocator.
+	T * allocate(const std::size_t n) const
+	{
+		// The return value of allocate(0) is unspecified.
+		// Mallocator returns NULL in order to avoid depending
+		// on malloc(0)'s implementation-defined behavior
+		// (the implementation can define malloc(0) to return NULL,
+		// in which case the bad_alloc check below would fire).
+		// All allocators can return NULL in this case.
+		if (n == 0) {
+			return NULL;
+		}
+ 
+		// All allocators should contain an integer overflow check.
+		// The Standardization Committee recommends that std::length_error
+		// be thrown in the case of integer overflow.
+		if (n > max_size())
+		{
+			throw std::length_error("aligned_allocator<T>::allocate() - Integer overflow.");
+		}
+ 
+		// Mallocator wraps malloc().
+		void * const pv = q_aligned_malloc(Alignment, n * sizeof(T));
+ 
+		// Allocators should throw std::bad_alloc in the case of memory allocation failure.
+		if (pv == NULL)
+		{
+			throw std::bad_alloc();
+		}
+ 
+		return static_cast<T *>(pv);
+	}
+ 
+	void deallocate(T * const p, const std::size_t n) const
+	{
+		q_aligned_free(p);
+	}
+ 
+ 
+	// The following will be the same for all allocators that ignore hints.
+	template <typename U>
+	T * allocate(const std::size_t n, const U * /* const hint */) const
+	{
+		return allocate(n);
+	}
+ 
+ 
+	// Allocators are not required to be assignable, so
+	// all allocators should have a private unimplemented
+	// assignment operator. Note that this will trigger the
+	// off-by-default (enabled under /Wall) warning C4626
+	// "assignment operator could not be generated because a
+	// base class assignment operator is inaccessible" within
+	// the STL headers, but that warning is useless.
+private:
+	aligned_allocator& operator=(const aligned_allocator&);
+};
+
+template<typename T>
+using aligned_vector = std::vector<T, aligned_allocator<T, alignof(T)>>;
 
 inline RTCRayHit SetupRay(unsigned rayindex, const qvec3d &start, const qvec3d &dir, vec_t dist)
 {
@@ -200,27 +338,19 @@ inline const sceneinfo &Embree_SceneinfoForGeomID(unsigned int geomID)
 class raystream_intersection_t : public raystream_embree_common_t
 {
 private:
-    RTCRayHit *_rays = nullptr;
+    aligned_vector<RTCRayHit> _rays;
 
 public:
     inline raystream_intersection_t() = default;
 
     inline raystream_intersection_t(size_t maxRays)
-        : raystream_embree_common_t(maxRays), _rays{static_cast<RTCRayHit *>(
-                                                  q_aligned_malloc(16, sizeof(RTCRayHit) * maxRays))}
     {
+        resize(maxRays);
     }
-
-    inline ~raystream_intersection_t() { q_aligned_free(_rays); _rays = nullptr; }
 
     void resize(size_t size) override
     {
-        if (_rays) {
-            q_aligned_free(_rays);
-        }
-
-        _rays = static_cast<RTCRayHit *>(q_aligned_malloc(16, sizeof(RTCRayHit) * size));
-
+        _rays.resize(size);
         raystream_embree_common_t::resize(size);
     }
 
@@ -248,7 +378,7 @@ public:
             return;
 
         ray_source_info ctx2(this, self);
-        rtcIntersect1M(scene, &ctx2, _rays, _numrays, sizeof(_rays[0]));
+        rtcIntersect1M(scene, &ctx2, _rays.data(), _numrays, sizeof(_rays[0]));
     }
 
     inline qvec3d getPushedRayDir(size_t j)
@@ -297,27 +427,19 @@ public:
 class raystream_occlusion_t : public raystream_embree_common_t
 {
 private:
-    RTCRay *_rays = nullptr;
+    aligned_vector<RTCRay> _rays;
 
 public:
     inline raystream_occlusion_t() = default;
 
     inline raystream_occlusion_t(size_t maxRays)
-        : raystream_embree_common_t(maxRays), _rays{
-                                                  static_cast<RTCRay *>(q_aligned_malloc(16, sizeof(RTCRay) * maxRays))}
     {
+        resize(maxRays);
     }
-
-    inline ~raystream_occlusion_t() { q_aligned_free(_rays); }
 
     void resize(size_t size) override
     {
-        if (_rays) {
-            q_aligned_free(_rays);
-        }
-
-        _rays = static_cast<RTCRay *>(q_aligned_malloc(16, sizeof(RTCRay) * size));
-
+        _rays.resize(size);
         raystream_embree_common_t::resize(size);
     }
 
@@ -346,7 +468,7 @@ public:
             return;
 
         ray_source_info ctx2(this, self);
-        rtcOccluded1M(scene, &ctx2, _rays, _numrays, sizeof(_rays[0]));
+        rtcOccluded1M(scene, &ctx2, _rays.data(), _numrays, sizeof(_rays[0]));
     }
 
     inline bool getPushedRayOccluded(size_t j)
