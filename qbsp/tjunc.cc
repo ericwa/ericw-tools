c1168bc8ae93656347912a15dabf42bb4ca25e7b
Jonathan <jonno.5000@gmail.com>
2022-07-11 05:12:48 -0400

    fix tjunc output not actually outputting proper new vertices allow for basic rotation of face to find a good orientation

--- qbsp/tjunc.cc
+++ qbsp/tjunc.cc
@@ -27,7 +27,10 @@ size_t c_degenerate;
 size_t c_tjunctions;
 size_t c_faceoverflows;
 size_t c_facecollapse;
+#if 0
 size_t c_badstartverts;
+#endif
+size_t c_norotates, c_rotates;
 
 /*
 ==========
@@ -89,10 +92,10 @@ Forced a dumb check of everything
 */
 static void FindEdgeVerts(const qvec3d &, const qvec3d &, std::vector<size_t> &verts)
 {
-	verts.resize(map.bsp.dvertexes.size() - 1);
+	verts.resize(map.bsp.dvertexes.size());
 
 	for (size_t i = 0; i < verts.size(); i++) {
-		verts[i] = i + 1;
+		verts[i] = i;
 	}
 }
 
@@ -114,11 +117,6 @@ will be circularly filled in.
 inline void FaceFromSuperverts(node_t *node, face_t *f, size_t base, const std::vector<size_t> &superface)
 {
 	size_t remaining = superface.size();
-
-	// don't need to do any work
-	if (remaining <= MAXEDGES) {
-		return;
-	}
 
 	// split into multiple fragments, because of vertex overload
 	while (remaining > MAXEDGES) {
@@ -151,14 +149,13 @@ FixFaceEdges
 */
 static void FixFaceEdges (node_t *node, face_t *f)
 {
+#if 0
 	std::vector<size_t> count, start;
+#endif
 	std::vector<size_t> superface;
 	superface.reserve(64);
-	std::vector<size_t> edge_verts;
 
-	// brushbsp-fixme
-	//if (f->merged || f->split[0] || f->split[1])
-	//	return;
+	std::vector<size_t> edge_verts;
 
 	for (size_t i = 0; i < f->output_vertices.size(); i++) {
 		auto v1 = f->output_vertices[i];
@@ -172,10 +169,15 @@ static void FixFaceEdges (node_t *node, face_t *f)
 		vec_t len;
 		qvec3d edge_dir = qv::normalize(e2 - edge_start, len);
 
+#if 0
 		start.push_back(superface.size());
-		TestEdge(0, len, v1, v2, 0, edge_verts, edge_start, edge_dir, superface);
+#endif
 
+		TestEdge(0, len, v1, v2, 0, edge_verts, edge_start, edge_dir, superface);
+		
+#if 0
 		count.push_back(superface.size() - start[i]);
+#endif
 	}
 
 	if (superface.size() < 3) {
@@ -183,8 +185,59 @@ static void FixFaceEdges (node_t *node, face_t *f)
 		f->output_vertices.clear();
 		c_facecollapse++;
 		return;
+	} else if (superface.size() == f->output_vertices.size()) {
+		// face didn't need any new vertices
+		return;
 	}
 
+	// brute force rotating the start point until we find a valid winding
+	// that doesn't have any zero-area triangles
+	size_t i = 0;
+
+	for (; i < superface.size(); i++) {
+		size_t x = 0;
+
+		// try vertex i as the base, see if we find any zero-area triangles
+		for (; x < superface.size() - 2; x++) {
+			auto v0 = superface[i];
+			auto v1 = superface[(i + x + 1) % superface.size()];
+			auto v2 = superface[(i + x + 2) % superface.size()];
+
+            qvec3d d1 = map.bsp.dvertexes[v2] - map.bsp.dvertexes[v0];
+            qvec3d d2 = map.bsp.dvertexes[v1] - map.bsp.dvertexes[v0];
+            vec_t total = 0.5 * qv::length(qv::cross(d1, d2));
+
+			if (!total) {
+				break;
+			}
+		}
+
+		// found one!
+		if (x == superface.size() - 2) {
+			break;
+		}
+	}
+
+	if (i == superface.size()) {
+		// can't simply rotate to eliminate zero-area
+		c_norotates++;
+	} else if (i != 0) {
+		// have to rotate the superface to get it to work properly.
+		// use the face's output vertices as scratch space.
+		// brushbsp-todo: we can avoid this if we work directly on
+		// f->output_vertices in FaceFromSuperverts.
+		c_rotates++;
+
+		f->output_vertices.resize(superface.size());
+		// copy base -> end
+		auto out = std::copy(superface.begin() + i, superface.end(), f->output_vertices.begin());
+		// copy end -> base
+		std::copy(superface.begin(), superface.begin() + i, out);
+		// copy back to superface
+		superface = f->output_vertices;
+	}
+
+#if 0
 	// we want to pick a vertex that doesn't have tjunctions
 	// on either side, which can cause artifacts on trifans,
 	// especially underwater
@@ -205,9 +258,17 @@ static void FixFaceEdges (node_t *node, face_t *f)
 		// rotate the vertex order
 		base = start[i];
 	}
+#endif
 
-	// this may fragment the face if > MAXEDGES
-	FaceFromSuperverts(node, f, base, superface);
+	if (superface.size() > MAXEDGES) {
+		// split face into multiple faces if we're too large
+		FaceFromSuperverts(node, f, 0, superface);
+	} else {
+		// we aren't too large, so just move the superface right in
+		// brushbsp-todo: we can avoid this if we work directly on
+		// f->output_vertices in FaceFromSuperverts.
+		f->output_vertices = std::move(superface);
+	}
 }
 
 /*
@@ -246,7 +307,11 @@ void TJunc(node_t *headnode)
 	c_facecollapse = 0;
 	c_tjunctions = 0;
 	c_faceoverflows = 0;
+#if 0
 	c_badstartverts = 0;
+#endif
+	c_norotates = 0;
+	c_rotates = 0;
 
 	FixEdges_r (headnode);
 
@@ -254,5 +319,9 @@ void TJunc(node_t *headnode)
 	logging::print (logging::flag::STAT, "{:5} faces degenerated\n", c_facecollapse);
 	logging::print (logging::flag::STAT, "{:5} edges added by tjunctions\n", c_tjunctions);
 	logging::print (logging::flag::STAT, "{:5} faces added by tjunctions\n", c_faceoverflows);
+#if 0
 	logging::print (logging::flag::STAT, "{:5} bad start verts\n", c_badstartverts);
+#endif
+	logging::print (logging::flag::STAT, "{:5} faces unable to be rotated\n", c_norotates);
+	logging::print (logging::flag::STAT, "{:5} faces rotated\n", c_rotates);
 }
