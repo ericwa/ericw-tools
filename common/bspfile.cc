ericwa/ericw-tools

commit ddf2a36d39fcd9589ae950645d611740b611987d

qbsp: make func_detail_wall an alias for func_detail_fence

include/qbsp/qbsp.hh
@@ -101,7 +101,6 @@
 // Special contents flags for the compiler only
 #define CFLAGS_STRUCTURAL_COVERED_BY_DETAIL (1U << 0)
 #define CFLAGS_WAS_ILLUSIONARY           (1U << 1) /* was illusionary, got changed to something else */
-#define CFLAGS_DETAIL_WALL  (1U << 2) /* don't clip world for func_detail_wall entities */
 #define CFLAGS_BMODEL_MIRROR_INSIDE		 (1U << 3) /* set "_mirrorinside" "1" on a bmodel to mirror faces for when the player is inside. */
 #define CFLAGS_NO_CLIPPING_SAME_TYPE     (1U << 4) /* Don't clip the same content type. mostly intended for CONTENTS_DETAIL_ILLUSIONARY */
 

qbsp/brush.cc
@@ -1172,13 +1172,9 @@ Brush_LoadEntity(mapentity_t *dst, const mapentity_t *src, const int hullnum)
     if (!Q_strcasecmp(classname, "func_detail") && !options.fNodetail) {
         all_detail = true;
     }
-    if (!Q_strcasecmp(classname, "func_detail_wall") && !options.fNodetail) {
-        all_detail = true;
-        cflags |= CFLAGS_DETAIL_WALL;
-    }
     
     all_detail_fence = false;
-    if (!Q_strcasecmp(classname, "func_detail_fence") && !options.fNodetail) {
+    if ((!Q_strcasecmp(classname, "func_detail_fence") || !Q_strcasecmp(classname, "func_detail_wall")) && !options.fNodetail) {
         all_detail_fence = true;
     }
     
@@ -1233,9 +1229,7 @@ Brush_LoadEntity(mapentity_t *dst, const mapentity_t *src, const int hullnum)
             continue;
         
         /* -omitdetail option omits all types of detail */
-        if (options.fOmitDetail && detail && !(cflags & CFLAGS_DETAIL_WALL))
-            continue;
-        if ((options.fOmitDetail || options.fOmitDetailWall) && detail && (cflags & CFLAGS_DETAIL_WALL))
+        if (options.fOmitDetail && detail)
             continue;
         if ((options.fOmitDetail || options.fOmitDetailIllusionary) && detail_illusionary)
             continue;

qbsp/csg4.cc
@@ -637,14 +637,6 @@ CSGFaces(const mapentity_t *entity)
                 continue;
             }
             
-            if (clipbrush->contents == CONTENTS_DETAIL && (clipbrush->cflags & CFLAGS_DETAIL_WALL)
-                && !(brush->contents == CONTENTS_DETAIL && (brush->cflags & CFLAGS_DETAIL_WALL))) {
-                /* if clipbrush has CONTENTS_DETAIL and CFLAGS_DETAIL_WALL are set,
-                   only clip other brushes with both CONTENTS_DETAIL and CFLAGS_DETAIL_WALL.
-                 */
-                continue;
-            }
-            
             if (clipbrush->contents == CONTENTS_DETAIL_FENCE
                 && brush->contents != CONTENTS_DETAIL_FENCE) {
                 /* CONTENTS_DETAIL_FENCE never clips anything but itself */

qbsp/qbsp.cc
@@ -126,21 +126,17 @@ ProcessEntity(mapentity_t *entity, const int hullnum)
     {
         int solidcount = Brush_ListCount(entity->solid);
         int skycount = Brush_ListCount(entity->sky);
-        int detail_all_count = Brush_ListCount(entity->detail); /* including CFLAGS_DETAIL_WALL */
-        int detail_wall_count = Brush_ListCountWithCFlags(entity->detail, CFLAGS_DETAIL_WALL);
+        int detail_all_count = Brush_ListCount(entity->detail);
         int detail_illusionarycount = Brush_ListCount(entity->detail_illusionary);
         int detail_fence_count = Brush_ListCount(entity->detail_fence);
         int liquidcount = Brush_ListCount(entity->liquid);
     
         int nondetailcount = (solidcount + skycount + liquidcount);
-        int detailcount = detail_all_count - detail_wall_count;
+        int detailcount = detail_all_count;
         
         Message(msgStat, "%8d brushes", nondetailcount);
         if (detailcount > 0) {
             Message(msgStat, "%8d detail", detailcount);
-        }
-        if (detail_wall_count > 0) {
-            Message(msgStat, "%8d detail wall", detail_wall_count);
         }
         if (detail_fence_count > 0) {
             Message(msgStat, "%8d detail fence", detail_fence_count);

commit bfa7fea519be2d494074e27ec890186e724532d3

qbsp: restore func_detail_wall functionality
This reverts ddf2a36d

On further thought there was no real neeed to unify func_detail_wall
and func_detail_fence, and maps were using func_detail_wall for its optimization ability, so I want to preserve that.

e.g., ddf2a36d caused a subset of ad_tears to increase from 5200 to
5500 leafs.

common/bspfile.cc
@@ -99,6 +99,8 @@ struct gamedef_q1_like_t : public gamedef_t
     {
         if (contents.extended & CFLAGS_DETAIL) {
             return 5;
+        } else if (contents.extended & CFLAGS_DETAIL_WALL) {
+            return 5; // same priority as CFLAGS_DETAIL
         } else if (contents.extended & CFLAGS_DETAIL_FENCE) {
             return 4;
         } else if (contents.extended & CFLAGS_DETAIL_ILLUSIONARY) {
@@ -456,6 +458,8 @@ struct gamedef_q2_t : public gamedef_t
     {
         if (contents.extended & CFLAGS_DETAIL) {
             return 8;
+        } else if (contents.extended & CFLAGS_DETAIL_WALL) {
+            return 8; // same priority as CFLAGS_DETAIL
         } else if (contents.extended & CFLAGS_DETAIL_ILLUSIONARY) {
             return 6;
         } else if (contents.extended & CFLAGS_DETAIL_FENCE) {

include/common/bspfile.hh
@@ -88,6 +88,7 @@ enum extended_cflags_t : uint16_t
 {
     CFLAGS_STRUCTURAL_COVERED_BY_DETAIL = nth_bit(0u),
     CFLAGS_WAS_ILLUSIONARY = nth_bit(1), /* was illusionary, got changed to something else */
+    CFLAGS_DETAIL_WALL = nth_bit(2), /* don't clip world for func_detail_wall entities */
     CFLAGS_BMODEL_MIRROR_INSIDE = nth_bit(3), /* set "_mirrorinside" "1" on a bmodel to mirror faces for when the player is inside. */
     CFLAGS_NO_CLIPPING_SAME_TYPE = nth_bit(4), /* Don't clip the same content type. mostly intended for CONTENTS_DETAIL_ILLUSIONARY */
     // only one of these flags below should ever be set.
@@ -99,7 +100,7 @@ enum extended_cflags_t : uint16_t
     CFLAGS_DETAIL_FENCE = nth_bit(10),
     CFLAGS_ILLUSIONARY_VISBLOCKER = nth_bit(11),
     // all of the detail values
-    CFLAGS_DETAIL_MASK = (CFLAGS_DETAIL | CFLAGS_DETAIL_ILLUSIONARY | CFLAGS_DETAIL_FENCE),
+    CFLAGS_DETAIL_MASK = (CFLAGS_DETAIL | CFLAGS_DETAIL_ILLUSIONARY | CFLAGS_DETAIL_FENCE | CFLAGS_DETAIL_WALL),
     // all of the special content types
     CFLAGS_CONTENTS_MASK =
         (CFLAGS_HINT | CFLAGS_CLIP | CFLAGS_ORIGIN | CFLAGS_DETAIL_MASK | CFLAGS_ILLUSIONARY_VISBLOCKER)

include/qbsp/map.hh
@@ -214,6 +214,7 @@ struct brush_stats_t
     size_t liquid;
     size_t detail_fence;
     size_t detail;
+    size_t detail_wall;
     size_t sky;
     size_t solid;
 };

include/qbsp/qbsp.hh
@@ -382,7 +382,7 @@ struct surface_t
             lmshift = min(f->lmshift.front, f->lmshift.back);
 
             if (!((f->contents[0].extended | f->contents[1].extended) &
-                    (CFLAGS_DETAIL | CFLAGS_DETAIL_ILLUSIONARY | CFLAGS_DETAIL_FENCE | CFLAGS_WAS_ILLUSIONARY))) {
+                    (CFLAGS_DETAIL | CFLAGS_DETAIL_ILLUSIONARY | CFLAGS_DETAIL_FENCE | CFLAGS_DETAIL_WALL | CFLAGS_WAS_ILLUSIONARY))) {
                 has_struct = true;
             }
 

qbsp/brush.cc
@@ -790,7 +790,7 @@ std::optional<brush_t> LoadBrush(const mapentity_t *src, const mapbrush_t *mapbr
 // temporary brush lists to hold sorting data
 struct brush_types_t
 {
-    std::vector<brush_t> detail_illusionary, liquid, detail_fence, detail, sky, solid;
+    std::vector<brush_t> detail_illusionary, liquid, detail_fence, detail_wall, detail, sky, solid;
 };
 
 static brush_stats_t Entity_SortBrushes(mapentity_t *dst, brush_types_t &types)
@@ -802,12 +802,13 @@ static brush_stats_t Entity_SortBrushes(mapentity_t *dst, brush_types_t &types)
     stats.detail_illusionary = types.detail_illusionary.size();
     stats.liquid = types.liquid.size();
     stats.detail_fence = types.detail_fence.size();
+    stats.detail_wall = types.detail_wall.size();
     stats.detail = types.detail.size();
     stats.sky = types.sky.size();
     stats.solid = types.solid.size();
 
     dst->brushes.reserve(
-        stats.detail_illusionary + stats.liquid + stats.detail_fence + stats.detail + stats.sky + stats.solid);
+        stats.detail_illusionary + stats.liquid + stats.detail_fence + stats.detail_wall + stats.detail + stats.sky + stats.solid);
 
     dst->brushes.insert(dst->brushes.end(), make_move_iterator(types.detail_illusionary.begin()),
         make_move_iterator(types.detail_illusionary.end()));
@@ -815,6 +816,8 @@ static brush_stats_t Entity_SortBrushes(mapentity_t *dst, brush_types_t &types)
         dst->brushes.end(), make_move_iterator(types.liquid.begin()), make_move_iterator(types.liquid.end()));
     dst->brushes.insert(dst->brushes.end(), make_move_iterator(types.detail_fence.begin()),
         make_move_iterator(types.detail_fence.end()));
+    dst->brushes.insert(dst->brushes.end(), make_move_iterator(types.detail_wall.begin()),
+        make_move_iterator(types.detail_wall.end()));
     dst->brushes.insert(
         dst->brushes.end(), make_move_iterator(types.detail.begin()), make_move_iterator(types.detail.end()));
     dst->brushes.insert(dst->brushes.end(), make_move_iterator(types.sky.begin()), make_move_iterator(types.sky.end()));
@@ -830,7 +833,7 @@ static void Brush_LoadEntity(mapentity_t *dst, const mapentity_t *src, const int
     qvec3d rotate_offset{};
     int i;
     int lmshift;
-    bool all_detail, all_detail_fence, all_detail_illusionary;
+    bool all_detail, all_detail_fence, all_detail_illusionary, all_detail_wall;
 
     const std::string &classname = src->epairs.get("classname");
     /*
@@ -882,8 +885,13 @@ static void Brush_LoadEntity(mapentity_t *dst, const mapentity_t *src, const int
             all_detail = true;
         }
 
+        all_detail_wall = false;
+        if (!Q_strcasecmp(classname, "func_detail_wall")) {
+            all_detail_wall = true;
+        }
+
         all_detail_fence = false;
-        if (!Q_strcasecmp(classname, "func_detail_fence") || !Q_strcasecmp(classname, "func_detail_wall")) {
+        if (!Q_strcasecmp(classname, "func_detail_fence")) {
             all_detail_fence = true;
         }
 
@@ -933,12 +941,14 @@ static void Brush_LoadEntity(mapentity_t *dst, const mapentity_t *src, const int
         bool detail = false;
         bool detail_illusionary = false;
         bool detail_fence = false;
+        bool detail_wall = false;
         std::optional<bool> mirrorinside_brush = mirrorinside;
 
         // inherit the per-entity settings
         detail |= all_detail;
         detail_illusionary |= all_detail_illusionary;
         detail_fence |= all_detail_fence;
+        detail_wall |= all_detail_wall;
 
         if (!mirrorinside_brush) {
             if (options.target_game->id == GAME_QUAKE_II && (contents.native & (Q2_CONTENTS_AUX | Q2_CONTENTS_MIST))) {
@@ -957,6 +967,8 @@ static void Brush_LoadEntity(mapentity_t *dst, const mapentity_t *src, const int
             continue;
         if ((options.omitdetail.value() || options.omitdetailfence.value()) && detail_fence)
             continue;
+        if ((options.omitdetail.value() || options.omitdetailwall.value()) && detail_wall)
+            continue;
 
         /* turn solid brushes into detail, if we're in hull0 */
         if (hullnum <= 0 && contents.is_solid(options.target_game)) {
@@ -964,6 +976,8 @@ static void Brush_LoadEntity(mapentity_t *dst, const mapentity_t *src, const int
                 contents = {contents.native, CFLAGS_DETAIL_ILLUSIONARY};
             } else if (detail_fence) {
                 contents = {contents.native, CFLAGS_DETAIL_FENCE};
+            } else if (detail_wall) {
+                contents = {contents.native, CFLAGS_DETAIL_WALL};
             } else if (detail) {
                 contents = {contents.native, CFLAGS_DETAIL};
             }
@@ -1054,6 +1068,8 @@ static void Brush_LoadEntity(mapentity_t *dst, const mapentity_t *src, const int
             types.detail_illusionary.emplace_back(std::move(brush.value()));
         } else if (brush->contents.is_detail(CFLAGS_DETAIL_FENCE)) {
             types.detail_fence.emplace_back(std::move(brush.value()));
+        } else if (brush->contents.is_detail(CFLAGS_DETAIL_WALL)) {
+            types.detail_wall.emplace_back(std::move(brush.value()));
         } else {
             types.liquid.emplace_back(std::move(brush.value()));
         }

qbsp/csg4.cc
@@ -527,6 +527,11 @@ std::vector<surface_t> CSGFaces(const mapentity_t *entity)
                 continue;
             }
 
+            if (clipbrush.contents.is_detail(CFLAGS_DETAIL_WALL) && !brush.contents.is_detail(CFLAGS_DETAIL_WALL)) {
+                // if clipbrush is CFLAGS_DETAIL_WALL, only clip other brushes that are CFLAGS_DETAIL_WALL.
+                continue;
+            }
+
             if (clipbrush.contents.is_detail(CFLAGS_DETAIL_FENCE) && !brush.contents.is_detail(CFLAGS_DETAIL_FENCE)) {
                 /* CONTENTS_DETAIL_FENCE never clips anything but itself */
                 continue;

qbsp/qbsp.cc
@@ -838,6 +838,9 @@ static void ProcessEntity(mapentity_t *entity, const int hullnum)
     if (stats.detail) {
         logging::print(logging::flag::STAT, "     {:8} detail brushes\n", stats.detail);
     }
+    if (stats.detail_wall) {
+        logging::print(logging::flag::STAT, "     {:8} detail wall brushes\n", stats.detail_wall);
+    }
     if (stats.detail_illusionary) {
         logging::print(logging::flag::STAT, "     {:8} detail illusionary brushes\n", stats.detail_illusionary);
     }

qbsp/solidbsp.cc
@@ -36,7 +36,7 @@ std::atomic<int> splitnodes;
 
 static std::atomic<int> leaffaces;
 static std::atomic<int> nodefaces;
-static std::atomic<int> c_solid, c_empty, c_water, c_detail, c_detail_illusionary, c_detail_fence;
+static std::atomic<int> c_solid, c_empty, c_water, c_detail, c_detail_illusionary, c_detail_fence, c_detail_wall;
 static std::atomic<int> c_illusionary_visblocker;
 static bool usemidsplit;
 
@@ -76,6 +76,8 @@ void DetailToSolid(node_t *node)
             node->contents = options.target_game->create_solid_contents();
         } else if (node->contents.is_detail(CFLAGS_DETAIL_ILLUSIONARY)) {
             node->contents = options.target_game->create_empty_contents();
+        } else if (node->contents.is_detail(CFLAGS_DETAIL_WALL)) {
+            node->contents = options.target_game->create_solid_contents();
         }
         /* N.B.: CONTENTS_DETAIL_FENCE is not remapped to CONTENTS_SOLID until the very last moment,
          * because we want to generate a leaf (if we set it to CONTENTS_SOLID now it would use leaf 0).
@@ -648,6 +650,8 @@ static void LinkConvexFaces(std::vector<surface_t> &planelist, node_t *leafnode)
         c_illusionary_visblocker++;
     } else if (leafnode->contents.extended & CFLAGS_DETAIL_FENCE) {
         c_detail_fence++;
+    } else if (leafnode->contents.extended & CFLAGS_DETAIL_WALL) {
+        c_detail_wall++;
     } else if (leafnode->contents.extended & CFLAGS_DETAIL_ILLUSIONARY) {
         c_detail_illusionary++;
     } else if (leafnode->contents.extended & CFLAGS_DETAIL) {
@@ -826,6 +830,7 @@ node_t *SolidBSP(const mapentity_t *entity, std::vector<surface_t> &surfhead, bo
     c_detail = 0;
     c_detail_illusionary = 0;
     c_detail_fence = 0;
+    c_detail_wall = 0;
     c_illusionary_visblocker = 0;
     // count map surfaces; this is used when deciding to switch between midsplit and the expensive partitioning
     mapsurfaces = surfhead.size();
@@ -841,6 +846,7 @@ node_t *SolidBSP(const mapentity_t *entity, std::vector<surface_t> &surfhead, bo
     logging::print(logging::flag::STAT, "     {:8} detail leafs\n", c_detail.load());
     logging::print(logging::flag::STAT, "     {:8} detail illusionary leafs\n", c_detail_illusionary.load());
     logging::print(logging::flag::STAT, "     {:8} detail fence leafs\n", c_detail_fence.load());
+    logging::print(logging::flag::STAT, "     {:8} detail wall leafs\n", c_detail_wall.load());
     logging::print(logging::flag::STAT, "     {:8} illusionary visblocker leafs\n", c_illusionary_visblocker.load());
     logging::print(logging::flag::STAT, "     {:8} leaffaces\n", leaffaces.load());
     logging::print(logging::flag::STAT, "     {:8} nodefaces\n", nodefaces.load());

qbsp/writebsp.cc
@@ -34,7 +34,7 @@ using nlohmann::json;
 
 static contentflags_t RemapContentsForExport(const contentflags_t &content)
 {
-    if (content.extended & CFLAGS_DETAIL_FENCE) {
+    if ((content.extended & CFLAGS_DETAIL_FENCE) || (content.extended & CFLAGS_DETAIL_WALL)) {
         /*
          * A bit of a hack for Q2, to ensure that structural faces which are completely covered by CFLAGS_DETAIL_FENCE
          * still render.